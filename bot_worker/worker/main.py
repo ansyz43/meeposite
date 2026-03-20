090032335ece58cded1bdcc2e063ec9f8ebe2c5c
Your Name <kisuke43@gmai.com>
2026-03-20 14:44:26 +1000

    fix: force IPv4 for Telegram API (IPv6 broken in Docker)

--- bot_worker/worker/main.py
+++ bot_worker/worker/main.py
@@ -9,10 +9,13 @@ import asyncio
 import hashlib
 import logging
 import datetime
+import socket
 import time
 from collections import OrderedDict
 
+import aiohttp
 from aiogram import Bot, Dispatcher, types, F
+from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.filters import CommandStart
 from sqlalchemy import select, update, func
 
@@ -370,7 +373,9 @@ async def start_bot(bot_record: BotModel, seller_name: str):
     while True:
         try:
             token = decrypt_token(bot_record.bot_token_encrypted)
-            bot = Bot(token=token)
+            connector = aiohttp.TCPConnector(family=socket.AF_INET)
+            session = AiohttpSession(connector=connector)
+            bot = Bot(token=token, session=session)
             dp = create_dispatcher(
                 bot_db_id=bot_record.id,
                 assistant_name=bot_record.assistant_name,
